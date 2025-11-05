094060d886be7e62ca97f023eb235f357f79adec
sunantongsan <104156645+sunantongsan@users.noreply.github.com>
2025-11-05 21:58:30 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ TEAM_WALLET = 'UQAfUj__LHqowEqTCKk2xaTFG8L2t_Pv46BiezvgeBnyRVTa'
 
 @app.post('/verify-transaction')
 async def verify_transaction(tx_id: str, user_address: str, ton_amount: float):
-    wallet = WalletV4R2(client, mnemonic='YOUR_MNEMONIC')  # แทนที่ด้วย mnemonic ของทีม
+    wallet = WalletV4R2(client, mnemonic='worth woman absent fence pattern dirt tiger exhibit repair electric
+ estate sadness pledge poverty flower bench penalty emerge stage lonely
+ skate advance blouse dust')  # แทนที่ด้วย mnemonic ของทีม
     kuba_amount = ton_amount * 96700 + ton_amount * 9670  # 96,700 KUBA + 10% bonus
     await wallet.transfer(user_address, kuba_amount, 'EQDCCMpdq2lab20fVNcXTx44TrGfAnNDvWiFWt9wDfDUY5YT')
     return {'status': 'success', 'kuba_amount': kuba_amount}
